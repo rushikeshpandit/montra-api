98d71f736fd14f4da874e37d85ab9e0ba2b4804b
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2023-10-15 22:06:00 +0530

    - Added api to routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
 import { AuthController } from "../controllers/AuthController";
+import { AccountsController } from "../controllers/AccountsController";
 
 export class Routes {
 	public authController: AuthController = new AuthController();
+	public accountController: AccountsController = new AccountsController();
 
 	public routes(app: any): void {
 		app.route("/").get((req: Request, res: Response) => {
@@ -15,5 +17,8 @@ export class Routes {
 		app.route("/api/v1/login").post(this.authController.login);
 		app.route("/api/v1/me").get(this.authController.authenticate);
 		app.route("/api/v1/registration").post(this.authController.registration);
+
+		app.route("/api/v1/account").post(this.accountController.createAccount);
+		app.route("/api/v1/account").get(this.accountController.getAccountById);
 	}
 }
