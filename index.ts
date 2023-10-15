cb66ed45955d9cc49fd16be2d22afa6cdf6c869d
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2023-10-15 15:53:10 +0530

    - Added express server

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log('Hello typescript !')
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(
+	bodyParser.urlencoded({
+		extended: true,
+	})
+);
